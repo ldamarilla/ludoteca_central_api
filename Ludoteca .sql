25b10e544f7f18ea47a2bba2f1ff4765e5a80241
SantiagoGuzzardi <sguzzardi@fi.uba.ar>
2025-06-09 14:13:35 -0300

    Agrego columna de apellido y dni en la base

--- Ludoteca .sql
+++ Ludoteca .sql
@@ -1,19 +1,25 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
---
--- Host: localhost    Database: Ludoteca
--- ------------------------------------------------------
--- Server version	8.0.42-0ubuntu0.24.04.1
+CREATE DATABASE IF NOT EXISTS Ludoteca;
+USE Ludoteca;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `ADMIN`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ADMIN` (
+  `ID_ADMIN` int NOT NULL AUTO_INCREMENT,
+  `NOMBRE_ADMIN` varchar(255) NOT NULL,
+  `CONTRASENIA` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID_ADMIN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ADMIN`
+--
+
+LOCK TABLES `ADMIN` WRITE;
+/*!40000 ALTER TABLE `ADMIN` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ADMIN` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `CARRITO`
@@ -144,6 +150,8 @@ CREATE TABLE `USUARIO` (
   `DIRECCION` varchar(255) NOT NULL,
   `PISO` int NOT NULL,
   `TIMBRE` varchar(10) NOT NULL,
+  `APELLIDO` varchar(255) NOT NULL,
+  `DNI` int NOT NULL,
   PRIMARY KEY (`ID_USUARIO`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -166,4 +174,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-08 20:58:12
+-- Dump completed on 2025-06-09 13:58:06
