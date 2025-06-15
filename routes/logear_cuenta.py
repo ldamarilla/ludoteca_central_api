3ba4ad88d370cd37f9084813aee36a664a153a9a
tomascarabajal25 <tomascarabajal25@gmail.com>
2025-06-14 21:54:22 -0300

    separar ingreso usuario de admin

--- routes/logear_cuenta.py
+++ routes/logear_cuenta.py
@@ -8,7 +8,7 @@ loguear_cuenta_bp = Blueprint("loguear_cuenta", __name__)
 engine = create_engine(DATABASE_URI)
 
 @loguear_cuenta_bp.route("/", methods=["GET", "POST"])
-def iniciarSesion():
+def iniciarSesionAdmin():
     if request.method == "POST":
         try:
             email = request.form.get("email")
@@ -16,8 +16,8 @@ def iniciarSesion():
 
             # Verificar si existe un usuario con ese email y contraseña
             query1 = text("""
-                SELECT ID_USUARIO, NOMBRE FROM USUARIO 
-                WHERE EMAIL = :email AND CONTRASENIA = :contrasenia
+                SELECT ID_ADMIN, NOMBRE_ADMIN FROM ADMIN
+                WHERE EMAIL_ADMIN = :email AND CONTRASENIA = :contrasenia
             """)
             params = {"email": email, "contrasenia": contrasenia}
 
@@ -28,6 +28,33 @@ def iniciarSesion():
 
                 # Login exitoso
                 return redirect("vista-admin.html", nombre=result["NOMBRE"]), 200
+            else:
+                # Email o contraseña incorrectos
+                return render_template("error.html"), 401
+            
+        except Exception as e:
+            print("Error durante el login:", e)
+            return render_template("error.html"), 500
+
+def iniciarSesionUsuario():
+    if request.method == "POST":
+        try:
+            email = request.form.get("email")
+            contrasenia = request.form.get("contrasenia")
+
+            # Verificar si existe un usuario con ese email y contraseña
+            query1 = text("""
+                SELECT ID_USUARIO FROM USUARIO
+                WHERE EMAIL = :email AND CONTRASENIA = :contrasenia
+            """)
+            params = {"email": email, "contrasenia": contrasenia}
+
+            with engine.connect() as conn:
+                result = conn.execute(query1, params).fetchone()
+
+            if result:
+                # Login exitoso
+                return redirect("inicio-sesion-ingresada.html"), 200
             else:
                 # Email o contraseña incorrectos
                 return render_template("error.html"), 401
