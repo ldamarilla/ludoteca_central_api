f55255364098259a15fd578d664c696adc1afde1
Lucas Amarilla <ldamarilla@fi.uba.ar>
2025-06-16 18:25:22 -0300

    2.15 Add sql

--- ludoteca.sql
+++ ludoteca.sql
@@ -18,9 +18,30 @@ CREATE TABLE `PRODUCTOS` (
   `CATEGORIA_ID` int,
   PRIMARY KEY (`ID`),
   KEY `CATEGORIA_ID` (`CATEGORIA_ID`),
-  CONSTRAINT `fk_produto_categoria_id`
+  CONSTRAINT `fk_producto_categoria_id`
         FOREIGN KEY (`CATEGORIA_ID`)
         REFERENCES `CATEGORIAS` (`ID`)
         ON DELETE SET NULL
+);
+
+#COMPRAS
+CREATE TABLE `COMPRAS` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `FECHA` datetime NOT NULL,
+  `USUARIO_ID` int, -- Agregar not null y foreign key cuando sse agrega la tabla de usuarios
+  `FINALIZADA` boolean,
+  PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `COMPRAS_PRODUCTOS` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `PRODUCTO_ID` int NOT NULL,
+  `CANTIDAD` int NOT NULL,
+  `COMPRA_ID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `PRODUCTO_ID` (`PRODUCTO_ID`),
+  KEY `COMPRA_ID` (`COMPRA_ID`),
+  CONSTRAINT `fk_compras_productos_compra_id` FOREIGN KEY (`COMPRA_ID`) REFERENCES `COMPRAS` (`ID`) ON DELETE CASCADE
+  CONSTRAINT `fk_compras_productos_producto_id` FOREIGN KEY (`PRODUCTO_ID`) REFERENCES `PRODUCTOS` (`ID`) ON DELETE CASCADE
 );
 
