fba8a70506afd3e394958bff408bd3483ad37418
NicoHorlent <nmonteverde@fi.uba.ar>
2025-06-20 11:44:38 -0300

    insercion de imagenes bdd

--- ludoteca.sql
+++ ludoteca.sql
@@ -16,6 +16,7 @@ CREATE TABLE `PRODUCTOS` (
   `STOCK` int DEFAULT NULL,
   `DESCRIPCION` varchar(1000) NOT NULL,
   `CATEGORIA_ID` int,
+  `IMAGEN` VARCHAR(255),
   PRIMARY KEY (`ID`),
   KEY `CATEGORIA_ID` (`CATEGORIA_ID`),
   CONSTRAINT `fk_produto_categoria_id`
@@ -54,16 +55,18 @@ INSERT INTO `CATEGORIAS` (`NOMBRE`) VALUES
 ('Aprendizaje'),
 ('Estrategia');
 
-INSERT INTO `PRODUCTOS` (`NOMBRE`,`PRECIO`,`STOCK`,`DESCRIPCION`,`CATEGORIA_ID`) VALUES 
-('TEG', 4000, null, 'Juego de estrategia por turnos de partidas de larga duracion', 4),
-('Ajedrez', 2500, null, 'El clásico juego de mesa etcetc', 7),
-('Monopoly', 3000, null, 'Ideal familia blablabla', 2),
-('D&D', 5000, null, 'Partidas largas de rol etcecte', 5),
-('4 en linea', 1500, null, 'hundir barcos y tal', 7),
-('UNO', 1000, null, 'para perder amistades y fragmentar a tu familia', 2),
-('Pictionary', 3500, null, 'Juego de dibujo familiar', 2),
-('Carrera de mente', 2000, null, 'Preguntas desafiantasnda', 6),
-('Damas', 2250, null, 'clasico etctec', 7),
-('Ludo', 1750, null, 'juego de familia muy familiar', 2),
-('Backgammon', 2199, null, 'azar y estrategia', 7),
-('Scrabble', 3200, null, 'Forma palabras para ganar', 6);
+INSERT INTO `PRODUCTOS` (`NOMBRE`,`PRECIO`,`STOCK`,`DESCRIPCION`,`CATEGORIA_ID`, `IMAGEN`) VALUES 
+('TEG', 4000, null, 'Juego de estrategia por turnos de partidas de larga duracion', 4, 'images/gris.jpeg'),
+('Ajedrez', 2500, null, 'El clásico juego de mesa etcetc', 7, '/images/ajedrez.jpeg'),
+('Monopoly', 3000, null, 'Ideal familia blablabla', 2, 'images/gris.jpeg'),
+('D&D', 5000, null, 'Partidas largas de rol etcecte', 5, 'images/gris.jpeg'),
+('4 en linea', 1500, null, 'hundir barcos y tal', 7, 'images/gris.jpeg'),
+('UNO', 1000, null, 'para perder amistades y fragmentar a tu familia', 2, 'images/gris.jpeg'),
+('Pictionary', 3500, null, 'Juego de dibujo familiar', 2, 'images/gris.jpeg'),
+('Carrera de mente', 2000, null, 'Preguntas desafiantasnda', 6, 'images/gris.jpeg'),
+('Damas', 2250, null, 'clasico etctec', 7, 'images/gris.jpeg'),
+('Ludo', 1750, null, 'juego de familia muy familiar', 2, 'images/gris.jpeg'),
+('Backgammon', 2199, null, 'azar y estrategia', 7, 'images/gris.jpeg'),
+('Scrabble', 3200, null, 'Forma palabras para ganar', 6, 'images/gris.jpeg');
+
+
