41a368d3942cc9abd1f4395271ddd431441ee256
Santiago <sguzzardi@fi.uba.ar>
2025-06-26 16:01:38 -0300

    Cambios en la base de datos

--- ludoteca.sql
+++ ludoteca.sql
@@ -97,16 +97,16 @@ INSERT INTO `CATEGORIAS` (`NOMBRE`) VALUES
 ('Aprendizaje'),
 ('Estrategia');
 
-INSERT INTO JUEGOS (NOMBRE, PRECIO, STOCK, DESCRIPCION, RUBRO_ID) VALUES
-('TEG', 4000, NULL, 'Juego de estrategia por turnos donde los jugadores compiten por conquistar territorios en un mapa mundial. Ideal para sesiones largas y apasionantes entre amigos.', 4, 'images/teg.jpeg'),
-('Ajedrez', 2500, NULL, 'Clásico juego de mesa de origen milenario que enfrenta a dos jugadores en una batalla de lógica, concentración y estrategia pura.', 7,'images/ajedrez.jpeg'),
-('Monopoly', 3000, NULL, 'Divertido juego de compraventa de propiedades donde los jugadores compiten por construir imperios inmobiliarios y evitar la bancarrota.', 2, 'images/monopoly.png'),
+INSERT INTO `PRODUCTOS` (NOMBRE, PRECIO, STOCK, DESCRIPCION, RUBRO_ID) VALUES
+('TEG', 4000, 10, 'Juego de estrategia por turnos donde los jugadores compiten por conquistar territorios en un mapa mundial. Ideal para sesiones largas y apasionantes entre amigos.', 4, 'images/teg.jpeg'),
+('Ajedrez', 2500, 20, 'Clásico juego de mesa de origen milenario que enfrenta a dos jugadores en una batalla de lógica, concentración y estrategia pura.', 7,'images/ajedrez.jpeg'),
+('Monopoly', 3000, 30, 'Divertido juego de compraventa de propiedades donde los jugadores compiten por construir imperios inmobiliarios y evitar la bancarrota.', 2, 'images/monopoly.png'),
 ('D&D', 5000, NULL, 'Juego de rol narrativo con dados y personajes personalizados. Ideal para quienes disfrutan de la fantasía, la imaginación y las aventuras en grupo.', 5, 'images/dnd.jpeg'),
-('4 en línea', 1500, NULL, 'Juego rápido y sencillo donde dos jugadores intentan alinear cuatro fichas del mismo color antes que su oponente. Perfecto para todas las edades.', 7, 'images/4linea.jpeg'),
+('4 en línea', 1500, 5, 'Juego rápido y sencillo donde dos jugadores intentan alinear cuatro fichas del mismo color antes que su oponente. Perfecto para todas las edades.', 7, 'images/4linea.jpeg'),
 ('UNO', 1000, NULL, 'Juego de cartas dinámico y colorido, ideal para grupos. Las reglas simples esconden una divertida competencia cargada de sorpresas.', 2, 'images/uno.jpeg'),
-('Pictionary', 3500, NULL, 'Juego de mesa en el que los jugadores deben dibujar palabras o frases para que su equipo las adivine. Diversión asegurada para reuniones.', 2, 'images/pictionary.jpeg'),
+('Pictionary', 3500, 30, 'Juego de mesa en el que los jugadores deben dibujar palabras o frases para que su equipo las adivine. Diversión asegurada para reuniones.', 2, 'images/pictionary.jpeg'),
 ('Carrera de mente', 2000, NULL, 'Desafiante juego de preguntas y respuestas que pone a prueba tu conocimiento general y rapidez mental en múltiples categorías.', 6, 'images/carreramente.jpeg'),
-('Damas', 2250, NULL, 'Juego de estrategia tradicional para dos jugadores donde el objetivo es capturar todas las piezas del oponente moviéndote en diagonal.', 7, 'images/damas.png'),
+('Damas', 2250, 5, 'Juego de estrategia tradicional para dos jugadores donde el objetivo es capturar todas las piezas del oponente moviéndote en diagonal.', 7, 'images/damas.png'),
 ('Ludo', 1750, NULL, 'Clásico juego familiar en el que cada jugador compite por llevar sus fichas a la meta lanzando dados. Fácil de aprender y muy entretenido.', 2, 'images/ludo.jpeg'),
-('Backgammon', 2199, NULL, 'Juego de mesa de origen antiguo que combina azar y estrategia. El objetivo es ser el primero en sacar todas las fichas del tablero.', 7, 'images/backgammon.jpeg'),
+('Backgammon', 2199, 20, 'Juego de mesa de origen antiguo que combina azar y estrategia. El objetivo es ser el primero en sacar todas las fichas del tablero.', 7, 'images/backgammon.jpeg'),
 ('Scrabble', 3200, NULL, 'Juego de palabras cruzadas en el que se forman términos sobre un tablero para sumar puntos. Ideal para expandir vocabulario y divertirse.', 6, 'images/scrabble.jpeg');
